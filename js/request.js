1f99a323fd211e1741b05be8b0b59f09c1e7c960
Alex Tzeng <alxtz.tw@gmail.com>
2017-10-15 13:45:30 -0700

    intergreate the apis

--- js/request.js
+++ js/request.js
@@ -2,10 +2,17 @@ console.log('test')
 
 $(document).ready(function() {
   $('#submit').click(function functionName() {
-    console.log('try to submit')
-    $.post( "", function( data ) {
-      $( ".result" ).html( data );
-      alert( "Load was performed." );
+    let imgUrl = $('#imgUrl').val()
+    let helpText = $('#helpText').val()
+    $.ajax({
+      type: "POST",
+      url: 'http://35.199.156.92:8787/api/create',
+      data: {
+        imageUrl: imgUrl,
+        helpText: helpText,
+        buttonText: 'Somehting',
+        replyText: 'osmeoe'
+      }
     });
-  })
+  });
 });
